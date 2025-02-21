0558166d65733ef2f7ca22c4c09bb222cccb0423
Marcelo Ferreira <marcelo.conceicao1718@gmail.com>
2025-02-21 23:47:17 +0000

    Instanciando as classes

--- Exemplo/MainExemplo.java
+++ Exemplo/MainExemplo.java
@@ -4,7 +4,9 @@ public class MainExemplo {
 
     public static void main(String[] args) {
 
-
+        Cliente cliente1 = new Cliente("Marcelo", 18, "27-01-2004", "Débito");
+        Funcionario funcionario1 = new Funcionario("Claudio", 21, "l123", "Gerente", 123);
+        
         
     }
 }
