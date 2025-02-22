269b86f8acc586c53c6c868632e7fe672b75d6a6
Marcelo Ferreira <marcelo.conceicao1718@gmail.com>
2025-02-22 00:18:48 +0000

    imprimindo informações das classes

--- Exemplo/MainExemplo.java
+++ Exemplo/MainExemplo.java
@@ -7,9 +7,10 @@ public class MainExemplo {
         Cliente cliente1 = new Cliente("Marcelo", 18, "27-01-2004", "Débito");
         Funcionario funcionario1 = new Funcionario("Claudio", 21, "l123", "Gerente", 123);
         
+        System.out.println(cliente1.toString());
+        System.out.println(funcionario1.toString());
 
 
 
-        
     }
 }
